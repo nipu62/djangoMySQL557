92347e6edde7b8cfccbd80021107325d36522fb9
nipu62 <nipu62@gmail.com>
2025-04-17 15:59:07 -0500

    Initial commit

---
+++ library/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import ListView
+from .models import Book
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
